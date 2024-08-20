0576042679baf716ff93f2a1439f024f3718ed3e
Sikyung Kweon <104986866+kweonsikyung@users.noreply.github.com>
2024-08-21 04:24:40 +0900

    feat: chat & mentoring page (#3)
    * feat: chat & mentoring page
    
    * fix: listBox styling

--- app/src/app/_components/ListBox/index.tsx
+++ app/src/app/_components/ListBox/index.tsx
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ListBox.module.css";
-import {
-  FaStar
-} from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+
 interface ListBoxProps {
   icon?: React.ReactNode;
   title: string;
   caption: string;
   desc: string;
-  onClick : ()=>void;
+  onClick: () => void;
+  isSelected: boolean;
 }
 
-const ListBox = ({ icon, title,caption, desc, onClick }: ListBoxProps) => {
+const ListBox = ({
+  icon,
+  title,
+  caption,
+  desc,
+  onClick,
+  isSelected,
+}: ListBoxProps) => {
   return (
-    <div className={styles.container} onClick={onClick}>
+    <div
+      className={`${styles.container} ${isSelected ? styles.selected : ""}`}
+      onClick={onClick}
+    >
       <div className={styles.top}>
-        <div className={styles.icon}><FaStar /></div>
+        <div className={styles.icon}>
+          <FaStar />
+        </div>
         <div>
           <div className={styles.title}>{title}</div>
           <div className={styles.caption}>{caption}</div>
         </div>
       </div>
-     <div className={styles.desc}>{desc}</div>
+      <div className={styles.desc}>{desc}</div>
     </div>
   );
 };
